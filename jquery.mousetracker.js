ae4eb235ed0b17912ebe60703c25684ae765cf52
Quentin <qschmick@gmail.com>
2015-02-12 08:08:37 -0500

    Create jquery.mousetracker.js

---
+++ jquery.mousetracker.js
@@ -0,0 +1,22 @@
+if(jQuery)
+    (function($){
+    
+        function init(input, settings){
+        }
+        
+        function saveMouseData(){
+        }
+        
+        function drawHeatMap(target){
+        }
+        
+        function decToHex(dec){
+        }
+        
+        function logMouse(e){
+        }
+        
+        function drawLiveContent(ticks, curNdx){
+        }
+    
+    })(jQuery);
